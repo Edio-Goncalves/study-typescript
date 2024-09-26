03787c22e3a7a42b4977674dd464bd18e45c39f6
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-26 18:58:59 -0300

    Lesson22: P.O.O. Inheritance public, Private and Protected (part2)

--- src/aula22.ts
+++ src/aula22.ts
@@ -1,2 +1,6 @@
 /* Herança com public, private e protected*/
 //https://www.youtube.com/watch?v=dWG_eeQiK68&list=PLx4x_zx8csUhtPMrkiGvFJVE5LX8Qat5s&index=22
+
+// public - acessado de qualquer lugar
+// private - acessado apenas por sua propria classe
+// protected - acessado na sua propria classe e classes filhos
---
+++ src/aula23.ts
@@ -0,0 +1,1 @@
+//https://www.youtube.com/watch?v=P7dk8J5-obs&list=PLx4x_zx8csUhtPMrkiGvFJVE5LX8Qat5s&index=23
