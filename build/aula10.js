30146ddde34e236c809b58bbefe6738024ce6613
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-26 13:44:36 -0300

    Lesson10: NULL, UNDEFINED e UNKNOWN

---
+++ build/aula10.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome10;
+nome10 = "Edio";
+let sobrenome10;
---
+++ src/aula10.ts
@@ -0,0 +1,11 @@
+/* NULL, UNDEFINED e UNKNOWN : nulo, indefinio e desconhecido*/
+
+// NULL é um tipo de valor vazio, esta indentificando que esta vazio com valor nulo (tipo nulo)
+let nome10: string | null;
+nome10 = "Edio";
+
+// UNDEFINED é um valor indefinido, o tipo é any mas o valor é indefinido (tipo indefinido)
+let sobrenome10;
+
+// UNKNOWN : só pode ser atribuido em tipos UNKNOWN e any
+//https://www.youtube.com/watch?v=n9KG1LJe4Gg&list=PLx4x_zx8csUhtPMrkiGvFJVE5LX8Qat5s&index=10
