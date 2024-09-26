449e8ad935b233a6951340849f680ff55d65ca62
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-26 16:02:00 -0300

    Lesson15: P.O.O. class

---
+++ build/aula16.js
@@ -0,0 +1,21 @@
+"use strict";
+class Boxes {
+}
+const box1 = new Boxes();
+const box2 = new Boxes();
+const box3 = new Boxes();
+class Computador {
+    nome = "";
+    ram = 0;
+    cpu = 0;
+    ligado = true;
+}
+const comp1 = new Computador();
+const comp2 = new Computador();
+const comp3 = new Computador();
+comp1.nome = "Bom";
+comp2.nome = "Ruim";
+comp3.nome = "Otimo";
+console.log(comp1.nome);
+console.log(comp2.nome);
+console.log(comp3.nome);
---
+++ src/aula16.ts
@@ -0,0 +1,27 @@
+/* P.O.O. */
+
+// A classe é um recipiente, um universo. Vamos sempre manipular os objetos.
+class Boxes {} // classe
+
+const box1 = new Boxes(); //objeto instanciado 01
+const box2 = new Boxes(); //objeto instanciado 02
+const box3 = new Boxes(); //objeto instanciado 03
+
+//
+class Computador {
+  nome: string = ""; // parametro é uma variavel dentro da classe
+  ram: number = 0;
+  cpu: number = 0;
+  ligado: boolean = true;
+}
+const comp1 = new Computador();
+const comp2 = new Computador();
+const comp3 = new Computador();
+
+comp1.nome = "Bom";
+comp2.nome = "Ruim";
+comp3.nome = "Otimo";
+
+console.log(comp1.nome);
+console.log(comp2.nome);
+console.log(comp3.nome);
